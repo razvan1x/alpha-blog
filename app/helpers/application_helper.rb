de12737b50bad139598e5f3103d27e0050838213
Razvan Cojei <razvanytb1@gmail.com>
2024-08-24 19:33:17 +0300

    Add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{hash}?s=#{size}&d=retro"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
