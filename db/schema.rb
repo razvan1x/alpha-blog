eda9befeac48a9eafdba90ddd6cd45c6227ed6cb
Razvan Cojei <razvanytb1@gmail.com>
2024-08-27 16:14:49 +0300

    Add boolean admin user field to app and admin user access to actions throughout application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_18_133226) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_27_122944) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_18_133226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
